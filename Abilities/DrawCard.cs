a7b487577d7c0637c14407c7cdd193dfb79c03ce
HH <haidarah134@gmail.com>
2022-01-30 15:41:40 +0700

    draw card

---
+++ Abilities/DrawCard.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Control.Core;
+using Attributes.Abilities;
+
+public class DrawCard : MonoBehaviour
+{
+    public int card = 1;
+    public GameObject effect;
+    static StatProcessor Calc = new StatProcessor();
+    public void Ability(BaseCreature caster, BaseCreature target, AbilityData Data)
+    {
+        PlayerController control = caster.transform.GetComponent<PlayerController>();
+        IEnumerator draw()
+        {
+            for (var x = 0;x<this.card;x++)
+            {
+                control.DeckAddTo(0);
+                yield return new WaitForSeconds(control.CardOutSpeed);
+            }
+        }
+        StartCoroutine(draw());
+    }
+    public string Text(AbilityData data, PlayerController caster, BaseCreature target)
+    {
+        return $"draw {this.card} card. ";
+    }
+    void Awake()
+    {
+        AbilityManager Mng = gameObject.GetComponent<AbilityManager>();
+        // Mng.ContainedAbilities.Add(this.Ability);
+        Mng.DescGrabber.Add(this.Text);
+    }
+}
