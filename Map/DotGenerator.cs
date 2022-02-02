3b3ca65568766b9b321f1952cf5443a08ff90786
HH <haidarah134@gmail.com>
2022-02-02 08:33:02 +0700

    distance calculated

--- Map/DotGenerator.cs
+++ Map/DotGenerator.cs
@@ -7,14 +7,16 @@ public class DotGenerator : MonoBehaviour
     public GameObject prefab;
     public Vector2 point1;
     public Vector2 point2;
+    public float gap;
     void Generate(Vector2 point1, Vector2 point2)
     {
         float progress = 0;
+        float distance = Vector2.Distance(point1, point2);
         while (progress<1)
         {
-            progress+= 0.1f;
             Vector2 pos = Vector2.Lerp(point1,point2,progress);
             Instantiate(prefab, pos, new Quaternion());
+            progress += gap / distance;
         }
     }
     public void onClick()
