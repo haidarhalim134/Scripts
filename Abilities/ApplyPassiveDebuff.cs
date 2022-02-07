8e45e3ffc709921b1045ed054acd1316ce6f84ac
HH <haidarah134@gmail.com>
2022-02-07 14:13:57 +0700

    integrate targeting

--- Abilities/ApplyPassiveDebuff.cs
+++ Abilities/ApplyPassiveDebuff.cs
@@ -10,13 +10,17 @@ namespace Attributes.Abilities
         [Tooltip("modifier")]
         public Debuffs type;
         public int charge;
+        public Targeting targeting;
         public ModType modType;
         public GameObject effect;
         static StatProcessor Calc = new StatProcessor();
         AbilityManager Mng;
         public void Ability(BaseCreature caster, BaseCreature target, AbilityData Data = null)
-        {
-            target.DebuffsAddPassive(this.type, this.charge);
+        {   
+            BaseCreature to;
+            if (targeting == Targeting.target) to = target;
+            else to = caster;
+            to.DebuffsAddPassive(this.type, this.charge);
             Animations.SpawnEffect(caster.gameObject, effect);
         }
         public string Text(AbilityData data,PlayerController caster, BaseCreature target)
