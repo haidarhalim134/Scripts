35154c1c0afd1f94068745eee67821bdcefb95e5
HH <haidarah134@gmail.com>
2022-02-12 07:47:14 +0700

    target marker

--- UI/CardHandlerVisual.cs
+++ UI/CardHandlerVisual.cs
@@ -22,18 +22,18 @@ namespace Control.UI
             public Vector2 Exit;
             void Awake()
             {
-            var trigger = this.GetComponent<EventTrigger>();
-            EventTrigger.Entry entry = new EventTrigger.Entry();
-            entry.eventID = EventTriggerType.PointerEnter;
-            entry.callback.AddListener((eventData) => { if (enableHover) Magnify(true,true); });
-            trigger.triggers.Add(entry);
-            entry = new EventTrigger.Entry();
-            entry.eventID = EventTriggerType.PointerExit;
-            entry.callback.AddListener((eventData) => { 
-                if(enableHover&&(transform.GetComponent<CardHandler>()==null||!transform.GetComponent<CardHandler>().Active))
-                Magnify(false,true); 
-            });
-            trigger.triggers.Add(entry);
+                var trigger = this.GetComponent<EventTrigger>();
+                EventTrigger.Entry entry = new EventTrigger.Entry();
+                entry.eventID = EventTriggerType.PointerEnter;
+                entry.callback.AddListener((eventData) => { if (enableHover) Magnify(true,true); });
+                trigger.triggers.Add(entry);
+                entry = new EventTrigger.Entry();
+                entry.eventID = EventTriggerType.PointerExit;
+                entry.callback.AddListener((eventData) => { 
+                    if(enableHover&&(transform.GetComponent<CardHandler>()==null||!transform.GetComponent<CardHandler>().Active))
+                    Magnify(false,true); 
+                });
+                trigger.triggers.Add(entry);
             }
             public void UpdateText(PlayerController caster = null, BaseCreature target = null)
             {
