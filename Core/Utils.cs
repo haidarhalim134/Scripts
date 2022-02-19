3df42e0ca20b59d93b5d0edcf35505d505e9ad5f
HH <haidarah134@gmail.com>
2022-02-19 10:10:48 +0700

    raycastt utils

---
+++ Core/Utils.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;  
+
+public class Utils
+{
+    public static List<RaycastResult> RaycastMouse()
+    {
+
+        PointerEventData pointerData = new PointerEventData(EventSystem.current)
+        {
+            pointerId = -1,
+        };
+
+        pointerData.position = Input.mousePosition;
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerData, results);
+
+        Debug.Log(results.Count);
+
+        return results;
+    }
+    public static T FindRayCastContaining<T>(List<RaycastResult> result)
+    {
+        if (result.Count==0)return default(T);
+        T res = result.Find((obj)=>obj.gameObject.GetComponent<T>()!=null).gameObject.GetComponent<T>();
+        if (res==null)Debug.Log("no containing");
+        return res;
+    }
+}
