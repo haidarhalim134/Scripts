75fbf8f1f54b7d48e24cf54c2a2a7056f3f4ae74
HH <haidarah134@gmail.com>
2022-02-20 19:28:52 +0700

    endturn button color

--- Buttons/CombatScene/EndTurnButton.cs
+++ Buttons/CombatScene/EndTurnButton.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 using Control.Core;
 
 public class EndTurnButton : MonoBehaviour
@@ -10,18 +12,15 @@ public class EndTurnButton : MonoBehaviour
     public void Disable()
     {
         this.Clickable = false;
+        this.GetComponent<Image>().DOColor(new Color32(150, 150, 150, 255), 0.1f);
     }
     public void Enable()
     {
         this.Clickable = true;
+        this.GetComponent<Image>().DOColor(new Color32(255, 255, 255, 255), 0.1f);
     }
     public void onClick()
     {
         if (this.Clickable) this.Owner.FinishTurn();
-    }
-    void Update()
-    {
-        // TODO: add effect on disable and all the conditionals
-        Clickable = Owner.Control;
     }
 }
